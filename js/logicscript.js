eb37e4f1c6eea4da74e4f4b6f59696ae6deb31b7
ksergeyru <i@ksergey.ru>
2022-07-30 15:12:05 +0300

    готов

--- js/logicscript.js
+++ js/logicscript.js
@@ -1,6 +1,13 @@
 function name(params) {
-    
+
 }
 function some() {
-    
+
+}
+
+function sum() {
+    let value1 = parseInt(prompt("Введите первое число"));
+    let value2 = parseInt(prompt("Введите второе число"));
+    let result = value1 + value2;
+    alert("Сумма " + result);
 }
